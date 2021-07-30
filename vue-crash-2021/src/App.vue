78ee548c20d58748ca89315e58218ce12b8040b9
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-30 00:03:49 -0500

    :dart: Improve: agregar rutas en vue

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -1,98 +1,47 @@
 <template>
   <div class="container">
-    <Header title="Task tracker" @btn-click="toggleAddTask" :showAddTask="showAddTask"/>
-    <div v-show="showAddTask">
-      <AddTask @add-task="addTask"/>
-    </div>
-    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+    <Header
+      title="Task tracker"
+      @btn-click="toggleAddTask"
+      :showAddTask="showAddTask"
+    />
+
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Tasks from './components/Tasks.vue'
-import AddTask from './components/AddTask.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
-    Tasks,
-    AddTask
+    Footer,
   },
-  data(){
+  data() {
     return {
-      tasks:[],
-      showAddTask: false
-    }
+      showAddTask: false,
+    };
   },
-  methods:{
-    async deleteTask(id){
-      if(confirm('Are you sure?')){
-        const res = await fetch(`api/tasks/${id}`,{
-          method:'DELETE'
-        })
-        res.status  === 200 ? (
-          this.tasks = this.tasks.filter(task => task.id !== id)
-        ) : alert('Error deleting task')
-      }
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
     },
-    async toggleReminder(id){
-      const taskToToggle  = await this.fetchTask(id)
-      const updTask = {...taskToToggle, reminder:!taskToToggle.reminder}
-
-      const res = await fetch(`api/tasks/${id}`,{
-        method:'PUT',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body: JSON.stringify(updTask)
-      })
-      const data = await res.json()
-      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder:data.reminder} :task)
-    },
-    async addTask(task){
-      const res = await fetch('api/tasks',{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
-
-        },
-        body: JSON.stringify(task)
-      })
-      const data = await res.json()
-      this.tasks = [...this.tasks, data]
-    },
-    toggleAddTask(){
-      this.showAddTask = !this.showAddTask
-    },
-    async fetchTasks(){
-      const res = await fetch('api/tasks')
-      const data = await res.json();
-
-      return data
-    },
-    async fetchTask(id){
-      const res = await fetch(`api/tasks/${id}`)
-      const data = await res.json();
-
-      return data
-    }
   },
-  async created(){
-    this.tasks = await this.fetchTasks()
-  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
---
+++ vue-crash-2021/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go home</router-link>
+
+</div>
+</template>

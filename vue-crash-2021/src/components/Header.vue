dd10edae569061dd8bc1d4ffc44cb9f1f38ef16c
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-30 00:07:29 -0500

    :dart: Improve: agregar restriccion de componente por ruta

--- vue-crash-2021/src/components/Header.vue
+++ vue-crash-2021/src/components/Header.vue
@@ -1,32 +1,46 @@
 <template>
-    <header>
-        <h1>{{title}}</h1>
-        <Button @btn-click="$emit('btn-click')" :text="showAddTask ? 'Close' : 'Add Task' " :color="showAddTask ? 'red' : 'green' " />
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('btn-click')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
 </template>
 
 <script>
-import Button from './Button.vue'
+import Button from "./Button.vue";
 export default {
-    name: 'Header',
-    props: {
-        title:{
-            type:String,
-            default:"Hello world"
-        },
-        showAddTask: Boolean
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello world",
     },
-    components:{
-        Button
-    }
-}
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-    header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
-    }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
